85e33343f9791e4395d73feb1c00ea0d0a231e6d
zc00herc <96848546+zc00herc@users.noreply.github.com>
2024-10-18 11:50:28 -0700

    cleaned up

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ from datetime import datetime
 import requests
 import os
 
-
 header = {
     'x-app-id': os.getenv('APP_ID'),
     'x-app-key': os.getenv('API_KEY'),
     'Authorization': os.getenv('SHEETY_TOKEN')
 }
-
-
 
 entry = input("Enter your workout: ")
 exercise_parmeters = {
@@ -20,9 +17,6 @@ exercise_parmeters = {
 }
 
 exercise_endpoint = 'https://trackapi.nutritionix.com/v2/natural/exercise'
-
-sheety_get_endpoint = 'https://api.sheety.co/7a68b96f49d65cd1d3b0ef4965b2ae87/workoutTracking/workouts'
-sheety_post_endpoint = 'https://api.sheety.co/7a68b96f49d65cd1d3b0ef4965b2ae87/workoutTracking/workouts'
 
 workout_response = requests.post(url=exercise_endpoint,headers=header,json=exercise_parmeters)
 workout_response.raise_for_status()
@@ -40,7 +34,7 @@ for index in range(0,len(returned_workout["exercises"])):
         }
     }
 
-    sheety_response = requests.post(sheety_post_endpoint,json=data_to_add,headers=header)
+    sheety_response = requests.post(os.getenv('sheety_post_endpoint'),json=data_to_add,headers=header)
     # sheety_response = requests.get(sheety_get_endpoint)
     sheety_response.raise_for_status()
     # print(sheety_response.text)
